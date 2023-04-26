fad2768487b8a3e63180760fcebc3a7207809c65
gtrosclairgtrosclairgtrosclair <gtrosclair@ithaca.edu>
2023-04-26 15:21:16 -0400

    image_to_text works with the first test image

--- image_to_text.py
+++ image_to_text.py
@@ -1,9 +1,7 @@
 import requests
 
 api_url = 'https://api.api-ninjas.com/v1/imagetotext'
-image_file_descriptor = open('YOUR_IMAGE.jpeg', 'rb')
+image_file_descriptor = open('images/74cd5f7c132ba6c02cec6e3ea6d981b18c8e26e44f566325198d613040583e5e_3.jpeg', 'rb')
 files = {'image': image_file_descriptor}
-r = requests.post(api_url, files=files)
+r = requests.post(api_url, files=files, headers={'X-Api-Key': 'qzxHLFBxoKXQDcfA8RowVQ==bhFZlshUidUT3Vbf'})
 print(r.json())
-
-#wut pls
