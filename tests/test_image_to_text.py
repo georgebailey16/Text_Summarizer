79622d566674cfceec25278e183b0b7c09dca0d7
George Bailey <gbailey@ithaca.edu>
2023-05-01 17:16:06 -0400

    print_image working basically

--- tests/test_image_to_text.py
+++ tests/test_image_to_text.py
@@ -11,4 +11,8 @@ class ImageText(unittest.TestCase):
         test_image = Image("Steezus Christ", "../images/74cd5f7c132ba6c02cec6e3ea6d981b18c8e26e44f566325198d613040583e5e_3.jpeg")
         self.assertEqual(14, len(test_image.get_image_text()))
         test_image.print_image()
+        test_image = Image("Comp 172 Syllabus", "../images/Screen Shot 2023-05-01 at 4.56.47 PM.png")
+        print(test_image.get_image_text())
+        # self.assertEqual(226, len(test_image.get_image_text()))
+        # test_image.print_image()
 
